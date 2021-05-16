f83e6f14a6d66c7ef7efef396f4af683a37ca3da
Sotniko89git config --global user.name Sotniko89git config --global user.name Sotniko89 <sotnikovvu@gmail.com>
2021-05-16 23:50:23 +0300

    add liquibase

--- src/main/java/ru/forum/ForumApplication.java
+++ src/main/java/ru/forum/ForumApplication.java
@@ -1,12 +1,22 @@
 package ru.forum;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ForumApplication {
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
+    }
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 }
