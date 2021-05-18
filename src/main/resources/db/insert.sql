3b6c7a7ada580393ce1c2de6c72f58603c642cc1
Sotniko89git config --global user.name Sotniko89git config --global user.name Sotniko89 <sotnikovvu@gmail.com>
2021-05-18 21:51:19 +0300

    add sql script tables manyToMany and insert Role

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,2 +1,3 @@
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
+insert into roles (name) values ('ROLE_USER');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -4,3 +4,18 @@ create table posts (
                        description text,
                        created timestamp without time zone not null default now()
 );
+create table users (
+                       id serial primary key,
+                       password varchar,
+                       username varchar,
+                       enabled boolean
+);
+create table roles (
+                       id serial primary key,
+                       name varchar
+);
+create table users_roles (
+                        id serial primary key,
+                        user_id int not null references users(id),
+                        roles_id int not null references roles(id)
+);
